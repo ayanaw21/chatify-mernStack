5455630e2f792aca26e9f20b19ab5fd24fd41188
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-11-18 03:31:01 -0800

    feat: add chatify-project as a local dependency and enhance server setup for production

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,36 @@
-// const express = require('express')
-import express from "express"
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config();
 
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-const app = express()
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
-app.listen(PORT,()=>{
-    console.log(`Server is running on port http://localhost/${PORT}`)
-})
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+// Production setup
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/dist")));
+
+	// ✅ Fixed catch-all route
+	// app.get("/(.*)", (_, res) => {
+	// 	res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+	// });
+
+	app.use((_, res) => {
+		res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port http://localhost:${PORT}`);
+});
