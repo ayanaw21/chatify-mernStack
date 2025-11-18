bb39822d0f2c8d340d5bec2ad1d1d6cc7f8f45f0
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-11-18 01:58:39 -0800

    feat: initialize server with authentication and messaging routes
    - Added package.json for server configuration and dependencies
    - Created auth.route.js for handling signup, signin, and signout endpoints
    - Created message.route.js for handling send message endpoint
    - Implemented server.js to set up Express server and route handling

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+// const express = require('express')
+import express from "express"
+import dotenv from 'dotenv'
+dotenv.config()
+
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
+app.listen(PORT,()=>{
+    console.log(`Server is running on port http://localhost/${PORT}`)
+})
