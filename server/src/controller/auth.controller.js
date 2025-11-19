99c2e056ab702d5d082fc254991c7ba987a1a657
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-11-19 12:50:36 -0800

    feat: add resend package and improve error handling in auth and db utilities

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -31,9 +31,12 @@ export const signUp = async (req, res) => {
 			password: hashedPassword,
 		});
 		if (newUser) {
-			generateToken(newUser._id, res);
-			await newUser.save(); 
+			// generateToken(newUser._id, res);
+			// await newUser.save(); 
 
+
+			const savedUser = await newUser.save();
+			generateToken(savedUser._id,res)
 			res.status(201).
 				json({
 					_id: newUser._id,
