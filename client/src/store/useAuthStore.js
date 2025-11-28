d6344b07e5cdb57884be8c66a34dc28530f2183e
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-11-28 02:47:56 -0800

    feat: add store functions to check auth

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set)=>({
+    authUser:null,
+    isCheckingAuth:true,
+    checkAuth:async ()=>{
+        try {
+            const res = await axiosInstance.get("/auth/check")
+            set({authUser:res.data})
+        } catch (error) {
+            console.log("Error in auth check",error)
+            set({authUser:null})
+        }
+        finally {
+            set({isCheckingAuth:false})
+        }
+    }
+}))
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors'
 import path from "path";
 dotenv.config();
 
@@ -8,6 +9,8 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from 'cookie-parser'
+
+
 const app = express();
 const __dirname = path.resolve();
 
@@ -15,6 +18,7 @@ const PORT =ENV.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use(cookieParser())
 // Routes
 app.use("/api/auth", authRoutes);
@@ -34,6 +38,7 @@ if (ENV.NODE_ENV === "production") {
 		res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 	});
 }
+
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port http://localhost:${PORT}`);
