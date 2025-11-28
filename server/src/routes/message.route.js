131849aa963993475f142da2c13d978cf9456a83
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-11-27 23:04:17 -0800

    feat: implement messaging functionality with message model, controller, and routes

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,10 +1,20 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+import {
+	getAllContacts,
+	getMessagesByUserId,
+	sendMessage,
+	getChatPartners,
+} from "../controller/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+const router = express.Router();
 
-router.get("/send",(req,res)=>{
-    res.send("Send message endpoint")
-})
+router.use(arcjetProtection,protectRoute)
 
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
 
+router.post("/send/:id", sendMessage);
 
-export default router
+export default router;
